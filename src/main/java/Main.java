2e7d9da8eb112141aea9a854750ad9f962e2a9d1
denisstocki <268439@student.pwr.edu.pl>
2022-10-19 23:02:01 +0200

    Denis: kolejny commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world !!!");
         System.out.println("Hello world changed by second user");
+        System.out.println("Hello world changed by third user");
     }
 }
